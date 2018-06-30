98f22d4ffecc0009f9a6cb371144a912e5916500
zliu32 <nflslzj@gmail.com>
2018-07-01 04:09:43 +0800

    blog submit function

--- myapp/src/app/blog/editor/editor.component.ts
+++ myapp/src/app/blog/editor/editor.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { SELECTEDATE } from '../../globals';
+import { journalInfo } from '../../entity/journalInterface';
+import * as global from "../../../app/globals";
+import { FcService } from '../../service/FcService';
+
+
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
+  providers: [FcService]
 })
 export class EditorComponent implements OnInit, DoCheck {
 
@@ -13,15 +19,16 @@ export class EditorComponent implements OnInit, DoCheck {
   public editorOptions = {
     placeholder: "insert content..."
   };
+  public showMessage: boolean;
+  public message: string;
+  public msgColor: string;
 
-  constructor() { }
+  constructor(private fcService: FcService) { }
 
   ngOnInit() {
-    this.editorContent = SELECTEDATE.date;
   }
 
   ngDoCheck() {
-    this.editorContent = SELECTEDATE.date;
   }
 
   onEditorBlured(quill) {
@@ -39,5 +46,25 @@ export class EditorComponent implements OnInit, DoCheck {
 
   onContentChanged({ quill, html, text }) {
     console.log('quill content is changed!', html);
+  }
+
+  submit() {
+    var id: string = SELECTEDATE.date + "-" + global.GlobalUserInfo.username;
+    var textInfo: journalInfo = {
+      id: id,
+      date: SELECTEDATE.date,
+      username: global.GlobalUserInfo.username,
+      context: this.editorContent
+    }
+    this.fcService.sendPost(textInfo, "/api/journal/save").subscribe(res => {
+      this.showMessage = true;
+      if (res !== undefined) {
+        this.message = "Successfully uploaded";
+        this.msgColor = "green";
+      } else {
+        this.msgColor = "red";
+        this.message = "Upload failed";
+      }
+    })
   }
 }
